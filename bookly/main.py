27925e737b88a4053aca38caf05f100552777244
Ervin Bosenbacher <ervin.bosenbacher@rvnllm.com>
2025-10-25 18:05:01 +0100

    adding new path with path variable

--- bookly/main.py
+++ bookly/main.py
@@ -5,7 +5,11 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict:
     return {"status": "OK", "response": "Hello" }
 
+
+@app.get("/greet/{name}")
+async def greet(name: str) -> dict:
+    return {"status": "OK", "response": f"Hello, {name}"}
 
